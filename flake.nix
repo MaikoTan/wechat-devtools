386b9b95472933163234d85a761bfe9dd58e4b81
Maiko Tan <maiko.tan.coding@gmail.com>
2025-09-28 13:56:59 +0800

    fix: export default package with each system and renew the install phase

--- flake.nix
+++ flake.nix
@@ -1,67 +1,91 @@
 {
   description = "WeChat DevTools for Linux - Unofficial version";
 
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
   outputs =
     {
       self,
       nixpkgs,
       flake-utils,
     }:
-    let
-      pkgs = import nixpkgs {
-        system = "x86_64-linux";
-      };
-      pname = "wechat-devtools";
-      version = "1.06.2504010-2";
-      src = pkgs.fetchurl {
-        url = "https://github.com/msojocs/wechat-web-devtools-linux/releases/download/v${version}/WeChat_Dev_Tools_v${version}_x86_64_linux.AppImage";
-        sha256 = "AQggXU24U+JAXmXKcbpYJKbfce/JVEH+dkHA0tBfvIw=";
-      };
-    in
-    {
-      packages.x86_64-linux.default = pkgs.appimageTools.wrapType2 rec {
-        inherit pname version src;
-
-        extraPkgs =
-          pkgs: with pkgs; [
-            gnome2.GConf
-            xorg.libxkbfile
-          ];
-
-        extraInstallCommands =
-          let
-            appimageContents = pkgs.appimageTools.extractType2 {
-              inherit pname version src;
-            };
-          in
-          ''
-            install -m 755 -D ${appimageContents}/AppRun $out/bin/${pname}
-            patchShebangs $out/bin/${pname}
-            install -m 444 -D ${appimageContents}/io.github.msojocs.wechat_devtools.desktop \
-              $out/share/applications/wechat-devtools.desktop
-            install -m 444 -D ${appimageContents}/wechat-devtools.png \
-              $out/share/icons/hicolor/512x512/apps/wechat-devtools.png
-          '';
-
-        meta = with pkgs.lib; {
-          mainProgram = "${pname}";
-          platforms = [ "x86_64-linux" ];
-          categories = [
-            "Development"
-            "IDE"
-          ];
-          description = "WeChat DevTools for Linux - Unofficial version";
-          longDescription = ''
-            WeChat DevTools for Linux - Unofficial version
-          '';
-          homepage = "https://github.com/msojocs/wechat-web-devtools-linux";
-          license = with licenses; [
-            mit # The Packaging Script is under MIT License
-            unfree # WeChat DevTools is not open source software
-          ];
-          maintainers = with maintainers; [ MaikoTan ];
-          sourceProvenance = with pkgs.lib.sourceTypes; [ binaryNativeCode ];
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
         };
-      };
-    };
+        pname = "wechat-devtools";
+        version = "1.06.2504010-2";
+        src = pkgs.fetchurl {
+          url = "https://github.com/msojocs/wechat-web-devtools-linux/releases/download/v${version}/WeChat_Dev_Tools_v${version}_x86_64_linux.AppImage";
+          sha256 = "AQggXU24U+JAXmXKcbpYJKbfce/JVEH+dkHA0tBfvIw=";
+        };
+      in
+      {
+        packages.default = pkgs.appimageTools.wrapType2 rec {
+          inherit pname version src;
+
+          extraPkgs =
+            pkgs: with pkgs; [
+              inetutils
+              gnome2.GConf
+              gnome-shell
+              gtk3 glib xorg.libX11 xorg.libXcomposite xorg.libXrandr xorg.libxshmfence
+              fontconfig
+              noto-fonts noto-fonts-cjk-sans noto-fonts-emoji
+            ];
+
+          nativeBuildInputs = [ pkgs.makeWrapper ];
+
+          profile = ''
+            export LC_ALL=zh_CN.UTF-8
+            export LANG=zh_CN.UTF-8
+          '';
+
+          extraInstallCommands =
+            let
+              appimageContents = pkgs.appimageTools.extractType2 {
+                inherit pname version src;
+              };
+            in
+            ''
+              cat $out/bin/${pname} << EOF
+              export LD_LIBRARY_PATH="${pkgs.glib}/lib:${pkgs.gtk3}/lib:${pkgs.xorg.libxshmfence}/lib:\$LD_LIBRARY_PATH"
+              # export PATH="${pkgs.glib}/bin:${pkgs.gtk3}/bin"
+              ${pkgs.bash} $out/AppRun
+              EOF
+              install -m 444 -D ${appimageContents}/io.github.msojocs.wechat_devtools.desktop \
+                $out/share/applications/wechat-devtools.desktop
+              substituteInPlace $out/share/applications/wechat-devtools.desktop \
+                --replace "Exec=bin/wechat-devtools" "Exec=${pname}"
+              install -m 444 -D ${appimageContents}/wechat-devtools.png \
+                $out/share/icons/hicolor/512x512/apps/wechat-devtools.png
+            '';
+
+          meta = with pkgs.lib; {
+            mainProgram = pname;
+            platforms = [ "x86_64-linux" ];
+            categories = [
+              "Development"
+              "IDE"
+            ];
+            description = "WeChat DevTools for Linux - Unofficial version";
+            longDescription = ''
+              WeChat DevTools for Linux - Unofficial version
+            '';
+            homepage = "https://github.com/msojocs/wechat-web-devtools-linux";
+            license = with licenses; [
+              mit # The Packaging Script is under MIT License
+              unfree # WeChat DevTools is not open source software
+            ];
+            maintainers = with maintainers; [ MaikoTan ];
+            sourceProvenance = with pkgs.lib.sourceTypes; [ binaryNativeCode ];
+          };
+        };
+      }
+    );
 }
