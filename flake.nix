db5800f143f446ae3d557dcce744d7ad088c21c4
Maiko Tan <maiko.tan.coding@gmail.com>
2025-10-22 15:02:28 +0800

    refactor: use steam-run with tar.xz package rather than appimage

--- flake.nix
+++ flake.nix
@@ -18,60 +18,83 @@
         pkgs = import nixpkgs {
           inherit system;
         };
+
         pname = "wechat-devtools";
-        version = "1.06.2504010-2";
-        src = pkgs.fetchurl {
-          url = "https://github.com/msojocs/wechat-web-devtools-linux/releases/download/v${version}/WeChat_Dev_Tools_v${version}_x86_64_linux.AppImage";
-          sha256 = "AQggXU24U+JAXmXKcbpYJKbfce/JVEH+dkHA0tBfvIw=";
+        version = "1.06.2504030-1";
+        src = builtins.fetchTarball {
+          url = "https://github.com/msojocs/wechat-web-devtools-linux/releases/download/v${version}/WeChat_Dev_Tools_v${version}_x86_64_linux.tar.gz";
+          sha256 = "sha256-w1pgR/Ay/dIUnBdMg2YPT03CDXBM5G6ZWGBwrK6xO8E=";
         };
-        appimageContents = pkgs.appimageTools.extractType2 {
-          inherit pname version src;
+
+        extraPkgs = pkgs: [
+          pkgs.nss
+          pkgs.libxshmfence
+        ];
+
+        steam-run =
+          (pkgs.steam.override {
+            inherit extraPkgs;
+          }).run;
+
+        wrapped = pkgs.writeShellScriptBin "wechat-devtools" ''
+          ${steam-run}/bin/steam-run ${pkgs.bash}/bin/bash ${src}/bin/wechat-devtools $@
+        '';
+
+        desktopItem = pkgs.makeDesktopItem {
+          name = "wechat-devtools";
+          exec = "${wrapped}/bin/wechat-devtools %U";
+          icon = builtins.fetchurl {
+            url = "https://github.com/msojocs/wechat-web-devtools-linux/raw/refs/heads/master/res/icons/wechat-devtools.svg";
+            sha256 = "sha256-QuL0itqB7Ki6NhMEhIh9jkRH3hJ3Upjj4zPZzEUYlkg=";
+          };
+          desktopName = "WeChat Devtools";
+          genericName = "WeChat Devtools";
+          comment = "The development tools for wechat web develop";
+          categories = [ "Development" "WebDevelopment" "IDE" ];
+          terminal = false;
+          startupWMClass = "nwjs_mbeenbnhnmdhkbicabncjghgnikfbgjh";
+          mimeTypes = [ "x-scheme-handler/wechatide" ];
+          extraConfig = {
+            "Name[zh_CN]" = "微信web开发者工具";
+          };
         };
       in
       {
-        packages.default = pkgs.appimageTools.wrapType2 rec {
-          inherit pname version src;
-
-          extraPkgs =
-            pkgs: with pkgs; [
-              inetutils
-              gnome2.GConf
-              gnome-shell
-              gtk3
-              glib
-              xorg.libX11
-              xorg.libXcomposite
-              xorg.libXrandr
-              xorg.libxshmfence
-              fontconfig
-              noto-fonts
-              noto-fonts-cjk-sans
-              noto-fonts-cjk-serif
-              noto-fonts-emoji
-            ];
+        packages.default = pkgs.stdenv.mkDerivation {
+          inherit pname version;
 
           nativeBuildInputs = [ pkgs.makeWrapper ];
 
-          profile = ''
-            export LC_ALL=zh_CN.UTF-8
-            export LANG=zh_CN.UTF-8
+          src = pkgs.symlinkJoin {
+            name = "wechat-devtools-wrapped";
+            paths = [ wrapped desktopItem ];
+          };
+
+          installPhase = ''
+            runHook preInstall
+
+            mkdir -p "$out/bin"
+            mkdir -p "$out/share/applications"
+            mkdir -p "$out/share/icons/hicolor/scalable/apps"
+
+            # Copy bin files using install to ensure new files are created
+            if [ -d "$src/bin" ]; then
+              for f in "$src/bin"/*; do
+                [ -e "$f" ] || continue
+                install -D -m755 "$f" "$out/bin/$(basename "$f")"
+              done
+            fi
+
+            # Copy shared data using tar pipeline to force content copy
+            if [ -d "$src/share" ]; then
+              mkdir -p "$out/share"
+              (cd "$src/share" && tar cf - .) | (cd "$out/share" && tar xf -)
+            fi
+
+            runHook postInstall
           '';
 
-          extraInstallCommands = ''
-              cat $out/bin/${pname} << EOF
-              export LD_LIBRARY_PATH="${pkgs.glib}/lib:${pkgs.gtk3}/lib:${pkgs.xorg.libxshmfence}/lib:\$LD_LIBRARY_PATH"
-              # export PATH="${pkgs.glib}/bin:${pkgs.gtk3}/bin"
-              ${pkgs.bash} $out/AppRun
-              EOF
-              install -m 444 -D ${appimageContents}/io.github.msojocs.wechat_devtools.desktop \
-                $out/share/applications/wechat-devtools.desktop
-              substituteInPlace $out/share/applications/wechat-devtools.desktop \
-                --replace "Exec=bin/wechat-devtools" "Exec=${pname}"
-              install -m 444 -D ${appimageContents}/wechat-devtools.png \
-                $out/share/icons/hicolor/512x512/apps/wechat-devtools.png
-            '';
-
-          meta = with pkgs.lib; {
+          meta = {
             mainProgram = pname;
             platforms = [ "x86_64-linux" ];
             categories = [
@@ -80,14 +103,15 @@
             ];
             description = "WeChat DevTools for Linux - Unofficial version";
             longDescription = ''
-              WeChat DevTools for Linux - Unofficial version
+              WeChat DevTools for Linux - Unofficial binary version providing development tools
+              for WeChat Mini Programs on Linux systems. This version uses the pre-compiled
+              binary distribution.
             '';
             homepage = "https://github.com/msojocs/wechat-web-devtools-linux";
-            license = with licenses; [
-              mit # The Packaging Script is under MIT License
-              unfree # WeChat DevTools is not open source software
-            ];
-            maintainers = with maintainers; [ MaikoTan ];
+            downloadPage = "https://github.com/msojocs/wechat-web-devtools-linux/releases";
+            changelog = "https://github.com/msojocs/wechat-web-devtools-linux/releases/tag/v${version}";
+            license = pkgs.lib.licenses.mit; # The packaging script is under MIT License
+            maintainers = [ pkgs.maintainers.maikotan ];
             sourceProvenance = with pkgs.lib.sourceTypes; [ binaryNativeCode ];
           };
         };
